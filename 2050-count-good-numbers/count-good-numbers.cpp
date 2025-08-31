5511db2777be4ed90802408f8a7580eeac930680
tanaymitra98 <tanaymitra01@gmail.com>
2025-08-31 22:40:31 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (36.40%) - LeetSync

---
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -0,0 +1,21 @@
+#define mod 1000000007
+class Solution {
+public:
+    long long power(long long x, long long y){
+        if(y==0) return 1;
+        long long ans = power(x,y/2);
+        ans *= ans;
+        ans %= mod;
+        if(y%2){
+            ans *= x;
+            ans %= mod;   // ensure modulo here
+        }
+        return ans;
+    }
+    
+    int countGoodNumbers(long long n) {
+        long long odd = n/2;
+        long long even = n/2 + n%2;
+        return (power(5,even) * power(4,odd)) % mod;  // apply modulo
+    }
+};
