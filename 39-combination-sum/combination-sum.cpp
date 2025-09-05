8b0d20ca146f93a1e35e7f3d305020626a204092
tanaymitra98 <tanaymitra01@gmail.com>
2025-09-05 19:06:54 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (90.70%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-    void getAllCombinations(vector<int>& candidates, int i, int target,
-                            vector<vector<int>> &ans, vector<int> &combi) {
-        // base case
-        if (target == 0) {
+    void getAllCombinations(vector<int> &candidates, int i, int target,vector<vector<int>> &ans,vector<int> &combi){
+        //base case
+        if(i == candidates.size() || target <0){
+            return;
+        }
+        if(target == 0){
             ans.push_back(combi);
             return;
         }
-        if (i == candidates.size() || target < 0) {
-            return;
-        }
-
-        // include current number
+     
         combi.push_back(candidates[i]);
-        getAllCombinations(candidates, i, target - candidates[i], ans, combi);
+        //get all combiantions
+        getAllCombinations(candidates, i , target - candidates[i], ans, combi);
         combi.pop_back();
 
-        // exclude current number
-        getAllCombinations(candidates, i + 1, target, ans, combi);
+        getAllCombinations(candidates, i+1 , target, ans, combi );
+
     }
+
+
+
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
         vector<int> combi;
-        getAllCombinations(candidates, 0, target, ans, combi);
+
+        getAllCombinations(candidates,0, target, ans, combi);
         return ans;
     }
 };
