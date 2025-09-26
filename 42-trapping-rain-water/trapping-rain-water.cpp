e745b51ee0a2201dac75be8bd06622bf91100c26
tanaymitra98 <tanaymitra01@gmail.com>
2025-09-26 20:18:25 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (30.88%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     vector<int> getLeftMax(vector<int> &height, int &n){
-        vector<int> left_max(n);
-        left_max[0] = height[0];
+        vector<int> left_max(n);// the array to be returned 
+        left_max[0] = height[0];//there wont be any left than the leftmost one 
 
-        for(int i = 1; i<n ; i++){
+        for(int i = 1; i<n ; i++){//iterate form the 2nd element
             left_max[i] = max(left_max[i-1],height[i]);
         }
         return left_max;
     }
 
     vector<int> getRightMax(vector<int> &height, int &n){
-        vector<int> right_max(n);
-        right_max[n-1] = height[n-1];
+        vector<int> right_max(n);//the array to be returned
+        right_max[n-1] = height[n-1];//there wont be any right than the right most element
 
         for(int i = n - 2; i>=0 ; i--){
             right_max[i] = max(right_max[i+1],height[i]);
