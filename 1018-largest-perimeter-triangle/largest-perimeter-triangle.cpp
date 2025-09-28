53e975bac438c04285dc518e578743ddb52d36b7
tanaymitra98 <tanaymitra01@gmail.com>
2025-09-29 03:40:51 +0530

    Time: 6 ms (51.77%) | Memory: 25.5 MB (95.24%) - LeetSync

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
@@ -8,7 +8,8 @@ public:
 
         for(int i = 0 ; i<nums.size() - 2 ; i++){
             if(nums[i] < nums[i+1] + nums[i+2]){
-                return nums[i] + nums[i+1] + nums[i+2];
+                sum =  nums[i] + nums[i+1] + nums[i+2];
+                return sum;
             }
         }
         return 0;
