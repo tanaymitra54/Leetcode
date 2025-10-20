c053866cc694c3cbeb9657f51e2ee4ffd0e95ee0
tanaymitra54 <144624301+tanaymitra54@users.noreply.github.com>
2025-10-20 14:19:13 +0530

    Time: 26 ms (12.89%) | Memory: 16.8 MB (12.12%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int longest = 0;
+        int n = s.length();
+        int l =0;
+        set<int> st;
+
+        for(int r =0 ; r<n ; r++){
+            while(st.find(s[r]) != st.end()){//the invalid case where the right pointer points to an element which  is in the string 
+                st.erase(s[l]);//rmeove l form the set 
+                l++;//increment the left pointer until its valid 
+            }
+
+            int w = (r-l)+1;
+            longest = max(longest, w);
+            st.insert(s[r]);
+        }
+        return longest;
+    }
+};
