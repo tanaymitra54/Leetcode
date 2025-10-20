4f04910647247ef0fab9e4529862c6fbb41ad1db
tanaymitra54 <144624301+tanaymitra54@users.noreply.github.com>
2025-10-20 14:24:24 +0530

    Time: 15 ms (38.54%) | Memory: 16.8 MB (11.25%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -8,12 +8,12 @@ public:
 
         for(int r =0 ; r<n ; r++){
             while(st.find(s[r]) != st.end()){//the invalid case where the right pointer points to an element which  is in the string 
-                st.erase(s[l]);//rmeove l form the set 
+                st.erase(s[l]);//rmeove the char at leftmost
                 l++;//increment the left pointer until its valid 
             }
 
-            int w = (r-l)+1;
-            longest = max(longest, w);
+            int w = (r-l)+1;//size of the window
+            longest = max(longest, w);//update  the longest and keep on comparing
             st.insert(s[r]);
         }
         return longest;
