5fa4923f796fd3c2a26e516ab15039f9ef7e1766
Tanay Mitra <144624301+tanaymitra54@users.noreply.github.com>
2026-02-18 23:39:39 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (62.63%) - LeetSync

---
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string toBinary(int n){
+        string binary = "";
+        while(n>0){
+            binary =char( '0' + n%2) +  binary;
+            n = n/2;
+        }
+        return binary;
+    }
+    bool hasAlternatingBits(int n) {
+        string s = toBinary(n);
+        for(int i =0 ; i<s.length(); i++){
+            if(s[i] == s[i+1]){
+                return false;
+            }
+        }
+        return true;
+
+    }
+};
