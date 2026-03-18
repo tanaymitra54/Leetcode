07a04aca2c6ec8651cca0a7e05376b369d4c5978
Tanay Mitra <144624301+tanaymitra54@users.noreply.github.com>
2026-03-18 23:34:09 +0530

    Time: 6 ms (73.87%) | Memory: 126.4 MB (94.97%) - LeetSync

--- 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.cpp
+++ 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
     bool canPartitionGrid(vector<vector<int>>& grid) {
-        int n = grid.size();    // rows
-        int m = grid[0].size(); // columns
-
-        long long total = 0;
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < m; j++)
-                total += grid[i][j];
-
-        // Try all horizontal splits (after row i)
-        long long topSum = 0;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < m; j++)
-                topSum += grid[i][j];
-            if (topSum * 2 == total) return true;
+        int n = grid.size();
+        int m = grid[0].size();
+        long long totalSum = 0;
+        for(int i =0 ; i<n; i++){
+            for(int j = 0; j<m ; j++){
+                totalSum += grid[i][j];
+            }
         }
 
-        // Try all vertical splits (after column j)
+        //upper/horizontal cut check
+        long long upperSum = 0;
+        for(int i = 0; i<n-1;i++){
+            for(int j =0;j<m; j++){
+                upperSum += grid[i][j];
+            }
+            if(upperSum*2 == totalSum) return true;
+        }
+
         long long leftSum = 0;
-        for (int j = 0; j < m - 1; j++) {
-            for (int i = 0; i < n; i++)
+        for(int j=0; j<m-1; j++){
+            for(int i = 0; i<n; i++){
                 leftSum += grid[i][j];
-            if (leftSum * 2 == total) return true;
+            }
+            if(leftSum*2 == totalSum) return true;
         }
 
         return false;
+
+
     }
 };
