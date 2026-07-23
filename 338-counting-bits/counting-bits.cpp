b6b002eb4a0ff16b23ee3cfc15eef97915c777bb
Tanay Mitra <144624301+tanaymitra54@users.noreply.github.com>
2026-07-24 02:13:55 +0530

    Time: 3 ms (23.10%) | Memory: 11.4 MB (8.64%) - LeetSync

---
+++ 338-counting-bits/counting-bits.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int binary(int num){
+        return num;  // no conversion needed, work directly on bits
+    }
+    int bits_one(int num){
+        int count = 0;
+        while(num>0){
+            int digit = num%2;      // changed %10 -> %2
+            if(digit == 1){
+                count++;
+            }
+            num = num/2;             // changed /10 -> /2
+        }
+        return count;
+    }
+    vector<int> countBits(int n) {
+        vector<int> ans;
+        for(int i =0 ; i<= n  ;i++){
+            int num = binary(i);
+            int bit_one = bits_one(num);
+            ans.push_back(bit_one);
+        }
+        return ans;
+
+    }
+};
