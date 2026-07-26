ba1b90ce6681328b3a41171dcb67cb0c43e959c5
Tanay Mitra <144624301+tanaymitra54@users.noreply.github.com>
2026-07-26 17:35:10 +0530

    Time: 13 ms (27.41%) | Memory: 31.7 MB (10.45%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
@@ -5,8 +5,8 @@ public:
 
         int n = nums.size();
        
-        int threeproduct = nums[n-1] * nums[n-2] * nums[n-3];
-        int twoSmallest = nums[0] * nums[1] * nums[n-1];
+        int threeproduct = nums[n-1] * nums[n-2] * nums[n-3];//multiply 3 largest number
+        int twoSmallest = nums[0] * nums[1] * nums[n-1];//two smallest numbers could be negative which on multiplication would give us a positive number
         
         return max(threeproduct,twoSmallest);
     }
