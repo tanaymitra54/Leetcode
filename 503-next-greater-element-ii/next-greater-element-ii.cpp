ba1927cf4489e3282986b08d777554f2b0132bb4
Tanay Mitra <144624301+tanaymitra54@users.noreply.github.com>
2026-08-03 13:59:44 +0530

    Time: 115 ms (5.00%) | Memory: 28.5 MB (64.82%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -5,7 +5,7 @@ public:
         vector<int> result;
         for(int i = 0 ; i<n; i++){
             bool found = false;
-            for(int j = 0 ; j<=n ; j++){
+            for(int j = 0 ; j<n ; j++){
                 int idx = (i+j)%n;
                 if(nums[idx] > nums[i]){
                     result.push_back(nums[idx]);
