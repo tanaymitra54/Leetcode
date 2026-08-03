be81a5dc995b736848884b3503ede83ba93ce3ab
Tanay Mitra <144624301+tanaymitra54@users.noreply.github.com>
2026-08-03 13:57:22 +0530

    Time: 112 ms (5.00%) | Memory: 28.6 MB (22.59%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        vector<int> result;
-        vector<int> nums2 = nums;
-
         int n = nums.size();
-
-        for(int i = 0; i< n; i++){
-            nums2.push_back(nums[i]);
-        }
-
-        for(int i =0 ; i<n; i++){
+        vector<int> result;
+        for(int i = 0 ; i<n; i++){
             bool found = false;
-            for(int j = i+1; j< i + n ; j++){
-                if(nums2[j]>nums[i]){
-                    result.push_back(nums2[j]);
+            for(int j = 0 ; j<=n ; j++){
+                int idx = (i+j)%n;
+                if(nums[idx] > nums[i]){
+                    result.push_back(nums[idx]);
                     found = true;
                     break;
                 }
-               
             }
             if(!found){
                 result.push_back(-1);
             }
         }
-
         return result;
     }
 };
